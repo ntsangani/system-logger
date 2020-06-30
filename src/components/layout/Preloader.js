bc9806abf25d3ff01c72e20cb1e4f5b349bdcd6a
Yonela <Ntsanganinela@gmailcom>
2020-06-30 12:09:03 +0200

    react

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Preloader = () => {
+    return (
+        <div className= "progress red lighten-4">
+            <div className="indeterminate red"/>
+        </div>
+    )
+}
+
+export default Preloader
+
